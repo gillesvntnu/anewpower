ceac0004e1e76b5c31d517497248cc5fc09ee356
Gilles Van De Vyver <gilles.van.de.vyver@ntnu.no>
2026-04-08 09:42:09 +0200

    Improve code reuse

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../App.css";
+import Footer from "../Footer";
 
 export const getScrollPosition = (sectionId: string): number => {
   const currentSection = document.getElementById(sectionId);
@@ -178,40 +179,7 @@ function Home(): JSX.Element {
         </div>
       </section>
 
-      {/* Footer */}
-      <footer className="site-footer">
-        <div className="footer-container">
-          <div className="footer-col">
-            <h4>ANewPower</h4>
-            <p>Uw partner voor zonnepanelen, batterijen en laadpalen.</p>
-          </div>
-          <div className="footer-col">
-            <h4>Diensten</h4>
-            <ul>
-              <li><Link to="/zonnepanelen">Zonnepanelen</Link></li>
-              <li><Link to="/thuisbatterijen">Thuisbatterijen</Link></li>
-              <li><Link to="/laadpalen">Laadpalen</Link></li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h4>Contact</h4>
-            <ul>
-              <li>
-                <i className="fas fa-map-marker-alt"></i> Kernenergiestraat 19 B16, 2610 Wilrijk
-              </li>
-              <li>
-                <i className="fas fa-envelope"></i> info@anewpower.be
-              </li>
-              <li>
-                <i className="fas fa-phone"></i> +32 477 21 43 33
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="footer-bottom">
-          <p>&copy; {new Date().getFullYear()} ANewPower. Alle rechten voorbehouden. Developer: <a href="https://gillesvandevyver.com" target="_blank" rel="noopener noreferrer">gillesvandevyver.com</a></p>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 }
